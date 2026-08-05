ea5cea2efcb8567ec48de563acba76c2d3c6c252
Harshitha-nayaka <llalithammak@gmail.com>
2026-08-05 12:33:36 +0530

    1st

---
+++ DAY2/lists.py
@@ -0,0 +1,7 @@
+stda=[["name","harshitha"],["age",21],["marks",[23,44,55]]]
+print(stda[0][0],":",stda[0][1])
+print(stda[1][0],":",stda[1][1])
+print(stda[2][0],":",)
+print(stda[2][1][0])
+print(stda[2][1][1])
+print(stda[2][1][2])
